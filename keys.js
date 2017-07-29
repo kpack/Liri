6d7470960bcf0caa60d5f1f4b5257bd13b4357f7
kpack <kevinpackler@gmail.com>
2017-07-28 23:43:20 -0400

    change

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'I8LvPuam8Ua3YI4iBJqZ8E1SM',
+  consumer_secret: 'dLNs6NhiELYCMLaLPdlRSxBvYHaFyHglwfOVQz2j0EgmmdHuhb',
+  access_token_key: '14766675-0dH3UMxPeabsAOSXEssjYll3Kff1UxnRWcGfrWxYy',
+  access_token_secret: 'vcsK4hNFYetaULDBL6vi69k9afJxWz7lsfPr5g4wle5Pc',
+}
+
+
+
